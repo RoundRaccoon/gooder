28a6987b69a74f9855f70faf3f77c08e7397611c
RoundRaccoon <ddaninn01@gmail.com>
2024-06-09 22:47:40 +0300

    Initialised navigation screen

---
+++ app/src/main/java/com/danielnastase/gooder/presentation/clients/NavigationScreen.kt
@@ -0,0 +1,14 @@
+package com.danielnastase.gooder.presentation.clients
+
+import androidx.compose.runtime.Composable
+import com.danielnastase.gooder.GooderAppState
+import com.danielnastase.gooder.ui.theme.GooderTheme
+
+@Composable
+fun NavigationScreen(
+    appState: GooderAppState
+) {
+    GooderTheme {
+        
+    }
+}
